2315dd23b393f51049a8ee36aa8dd20f4e2e16af
Ivan Zhirnov <zhirnov.works@gmail.com>
2022-12-09 22:38:20 +0400

    first commit

---
+++ assets/scripts/GameSettings.ts
@@ -0,0 +1,14 @@
+import { _decorator, Component, Node } from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass('GameSettings')
+export class GameSettings extends Component {
+    start() {
+
+    }
+
+    update(deltaTime: number) {
+        
+    }
+}
+
